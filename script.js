9aa99a06257ba52bd617eebd44a38ab8854a4e10
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-04-18 03:34:37 -0300

    sembonusesemscroll

--- script.js
+++ script.js
@@ -19,31 +19,65 @@ let message = {};
 let messagetext = "";
 let privado = {};
 let privadotext = "";
+let historico = [];
 
 function pegarmensagens(){
-  container.innerHTML ="";
   let promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
   promessa.then(postarmsg);
 }
+let checaprimeira = 0;
+let start = 0;
 
 function postarmsg(msg) {
-    for(let i=0; i< msg.data.length; i++){
-      if(msg.data[i].type === "status"){
-        statusmsg = msg.data[i];
-        poststatus();
-      }
-      if(msg.data[i].type === "message"){
+    if(checaprimeira === 0){
+      for(let i=0; i< msg.data.length; i++){
+        if(msg.data[i].type === "status"){
+          statusmsg = msg.data[i];
+          historico.push(msg.data[i]);
+          poststatus();
+        }
+        if(msg.data[i].type === "message"){
           message = msg.data[i];
+          historico.push(msg.data[i]);
           postmessage();
-      }
-      if(msg.data[i].type === "private_message"){
+        }
+        if(msg.data[i].type === "private_message"){
           if(msg.data[i].to === nome || msg.data[i].from === nome){
-          privado = msg.data[i];
-          postprivado();
+            privado = msg.data[i];
+            historico.push(msg.data[i]);
+            postprivado();
           }
+        }
       }
+      checaprimeira++;
+    }else {
+    start = 0;
+    for(let i=0; i< msg.data.length; i++){
+        if(start === 1){
+          if(msg.data[i].type === "status"){
+              statusmsg = msg.data[i];
+              historico.push(msg.data[i]);
+              poststatus();
+            }
+            if(msg.data[i].type === "message"){
+              message = msg.data[i];
+              historico.push(msg.data[i]);
+              postmessage();
+            }
+            if(msg.data[i].type === "private_message"){
+                if(msg.data[i].to === nome || msg.data[i].from === nome){
+                    privado = msg.data[i];
+                    historico.push(msg.data[i]);
+                    postprivado();
+                }
+            }  
+        }
+        if(msg.data[i].text === historico[historico.length-1].text && msg.data[i].to === historico[historico.length-1].to && msg.data[i].from === historico[historico.length-1].from && msg.data[i].time === historico[historico.length-1].time){
+          start = 1;
+        }
     }
-}
+    }
+} 
 pegarmensagens();
 setInterval(pegarmensagens, 3000);
 
@@ -52,8 +86,6 @@ function poststatus(){
     <h3>(${statusmsg.time})</h3> <h1>${statusmsg.from}</h1> <h2>${statusmsg.text}</h2>
     </div>`;
     container.innerHTML += statustext;
-    const statusaparece = document.querySelector('.statushtml');
-    statusaparece.scrollIntoView();
 }
 
 function postmessage(){
@@ -61,8 +93,6 @@ function postmessage(){
     <h3>(${message.time})  </h3> <h1> ${message.from} </h1> <h2> para </h2> <h1>${message.to}</h1><h2>: ${message.text}</h2>
     </div>`;
     container.innerHTML += messagetext;
-    const messageaparece = document.querySelector('.everyone');
-    messageaparece.scrollIntoView();
 }
 
 function postprivado(){
